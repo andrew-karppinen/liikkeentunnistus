d32ca3b1d9c6c1bc4bf3d7087a850a2d773ff613
andrew karppinen <99529988+andrew-karppinen@users.noreply.github.com>
2022-12-08 18:20:09 +0200

    Update raspi camera cv2.py

--- raspi camera cv2.py
+++ raspi camera cv2.py
@@ -7,7 +7,7 @@ from kuvienvertaus import KuvienVertaus
 
 '''
 Esimerkkiohjelma raspberry pi cameran käytöstä liiketunnistimena
-käyttää cv2 kirjastoa
+toteutettu cv2 kirjastolla
 
 ottaa kaksi kuvaa viiveellä
 tallentaa kuvat jos kuvissa riittävän suuri eroavaisuus
